5e5a2291eb736fa9f92d83addb0e09c6d851f2a9
Gabryel Duarte <gabryelduartepessanha@gmail.com>
2025-02-22 19:03:24 -0300

    testes

--- projeto/urls.py
+++ projeto/urls.py
@@ -16,7 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
 
+
+def my_view(request):
+        return HttpResponse("Home")
+   
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('/', my_view), #Home
 ]
